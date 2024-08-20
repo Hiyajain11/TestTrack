2e03d91fb16852ce2b4ef7aef2d41f6ce5d56605
Hiyajain11 <122627976+Hiyajain11@users.noreply.github.com>
2024-08-20 23:59:38 +0530

    Add files via upload

---
+++ pages/AboutUs.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function AboutUs() {
+  return <div>
+  <h1>About Us</h1>
+  <h3>Test Track is the ultimate online proctoring solution designed to ensure the integrity and fairness of every exam. <br />
+  With advanced monitoring technology, Test Track keeps a vigilant eye on test-takers, reminding them, "Hey there, don't try to cheat. <br />
+  Someone is watching you!" Say goodbye to sneaky cheaters and hello to fair exams, as Test Track creates a secure environment where honesty prevails. <br />
+  Whether you're a student or an educator, you can trust that your exams are in good hands with Test Track, making sure that every test is a true reflection of knowledge and effort.
+
+</h3>
+  </div>;
+}
+
+export default AboutUs;
